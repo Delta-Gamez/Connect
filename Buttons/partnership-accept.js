4c22371a38f21692ace0c42dc4b6ee80499ca879
NZ <88480788+DeltaGamingCH@users.noreply.github.com>
2024-05-31 08:54:16 +0200

    Finalized partnership-accepted embed

--- Buttons/partnership-accept.js
+++ Buttons/partnership-accept.js
@@ -25,8 +25,10 @@ module.exports = {
         }
         
         const embed = new EmbedBuilder(embedInfoSuccess.Template)
-            .setTitle("Partnership Accepted")
-            .setDescription(`Your partnership request has been accepted. Please wait for further instructions by staff, to complete the partnership.`)
+            .setTitle("<:DG_CO_Check:1028309734450806815>⠀Partnership Accepted")
+            .setDescription(`<@${user.user.id}>, your partnership has been accepted. Please wait for further instructions by the staff team to complete the partnership. \n⠀`)
+            .setThumbnail('https://cdn.discordapp.com/emojis/1172188410522386533.webp?size=22&quality=lossless')
+            .setFooter({ text: 'Connect', iconURL: 'https://cdn.discordapp.com/emojis/1203623412271022150.webp?size=80&quality=lossless' });
             .setTimestamp();
 
         await interaction.channel.setName(`${user.user.username} - Accepted`);
