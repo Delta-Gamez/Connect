9aa9368dbbc0af380312d24b6a1b0d338f5ff585
kezza2k7 <160131709+kezza2k7@users.noreply.github.com>
2024-06-04 19:56:46 +0000

    Update

--- Commands/manager.js
+++ Commands/manager.js
@@ -6,7 +6,7 @@ const { info, error } = require("../src/log.js");
 module.exports = {
     data: new SlashCommandBuilder()
         .setName("manager")
-        .setDescription("Staff Management Commands"),
+        .setDescription("Staff Management Commands"), // gtg home, ill be back in a bit
     async execute(interaction) {
         if (!interaction.guildId) {
             interaction.reply()
