44ef77f0274a4a7b635b348637dc9e0cca9716df
deltagamingch <deltagaming.ch.nik@gmail.com>
2024-06-14 10:10:21 +0200

    Updated embed GuildJoin, broken the functionality

--- src/events/GuildJoin.js
+++ src/events/GuildJoin.js
@@ -1,15 +1,22 @@
 const { custom } = require('../log.js');
 const { EmbedBuilder } = require("discord.js");
 
+const colorSuccess = '#45BB8A';
+
 module.exports = {
 	name: 'guildCreate',
 	execute: async(guild) => {
         console.log(guild)
         const user = await guild.members.fetch(guild.ownerId);
         const embed = new EmbedBuilder()
-            .setTitle("Guild Joined")
-            .setDescription(`Guild Data:\nID: ${guild.id}\nName: ${guild.name}\nOwner: ${user.user.username}(${guild.ownerId})\nMembers: ${guild.memberCount}\nBanner: ${guild.bannerURL()}`)
-            .setTimestamp();
+            .setTitle('GUILD JOINED')
+            .setColor(colorSuccess)
+            .setDescription(`
+                **Server**: ${guild.name}, ${guild.id}
+                **Server Owner**: ${user.user.username}(${guild.ownerId})
+                **Members:** ${guild.memberCount}`)
+            .setTimestamp()
+            .setFooter('Connect Logs');
 
         if (guild.iconURL()) {
             embed.setThumbnail(guild.iconURL());
